331ccf382aabe3f5847169d537ed4f4d5a33656f
Saurav Kumar <skbk104@gmail.com>
2022-03-21 10:17:04 +0530

    updated for zip.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import * as zip from "@zip.js/zip.js";
-var chilkat = require('@chilkat/ck-node16-win64');
+import * as zip from "@zip.js/zip.js";
+// var chilkat = require('@chilkat/ck-node16-win64');
 
 
 @Component({
@@ -39,29 +39,27 @@ export class AppComponent implements OnInit {
 
     // console.log(file);
 
-    var zip = new chilkat.Zip();
-    var success = zip.OpenZip("something.zip");
-    console.log('success : ', success);
-    if (success !== true) {
-      console.log(zip.LastErrorText);
-      return;
-    }
+    // var zip = new chilkat.Zip();
+    // var success = zip.OpenZip("something.zip");
+    // console.log('success : ', success);
+    // if (success !== true) {
+    //   console.log(zip.LastErrorText);
+    //   return;
+    // }
 
-    var bPwdProt = zip.PasswordProtect;
-    console.log('IsPassword : ', bPwdProt);
-    if (bPwdProt) {
-      console.log("This zip is password-protected.");
-    }
+    // var bPwdProt = zip.PasswordProtect;
+    // console.log('IsPassword : ', bPwdProt);
+    // if (bPwdProt) {
+    //   console.log("This zip is password-protected.");
+    // }
 
-    var encValue = zip.Encryption;
-    console.log('Encrypted value : ', encValue);
-    if (encValue == 4) {
-      console.log("This zip is AES encrypted.");
-    }
+    // var encValue = zip.Encryption;
+    // console.log('Encrypted value : ', encValue);
+    // if (encValue == 4) {
+    //   console.log("This zip is AES encrypted.");
+    // }
 
-    zip.CloseZip();
-
-
+    // zip.CloseZip();
 
 
 
@@ -70,64 +68,66 @@ export class AppComponent implements OnInit {
 
 
 
-    // file.arrayBuffer().then((arrayBuffer: ArrayBuffer) => {
-    //   const blob = new Blob([new Uint8Array(arrayBuffer)], { type: file.type });
-    //   // console.log(blob);
-    // })
 
-    // const fileToBlob = async (file: { arrayBuffer: () => any; type: any; }) => new Blob([new Uint8Array(await new Uint8Array(await file.arrayBuffer()))], { type: file.type });
-    // const blob = await fileToBlob(file)
 
-    // console.log(blob);
-    // this.verifyZipPassword(blob, this.password);
+    file.arrayBuffer().then((arrayBuffer: ArrayBuffer) => {
+      const blob = new Blob([new Uint8Array(arrayBuffer)], { type: file.type });
+      // console.log(blob);
+    })
+
+    const fileToBlob = async (file: { arrayBuffer: () => any; type: any; }) => new Blob([new Uint8Array(await new Uint8Array(await file.arrayBuffer()))], { type: file.type });
+    const blob = await fileToBlob(file)
+
+    console.log(blob);
+    this.verifyZipPassword(blob, this.password);
   }
 
 
-  // verifyZipPassword = async (blob: Blob, password: string) => {
-  //   this.filelist = [];
-  //   this.filelistToUI = [];
-  //   let reader: zip.ZipReader;
-  //   try {
+  verifyZipPassword = async (blob: Blob, password: string) => {
+    this.filelist = [];
+    this.filelistToUI = [];
+    let reader: zip.ZipReader;
+    try {
 
-  //     if(password!==undefined && password!==null && password!==''){
-  //       reader = new zip.ZipReader(new zip.BlobReader(blob),{password});
-  //     }else{
-  //       reader = new zip.ZipReader(new zip.BlobReader(blob));
-  //     }
-
-
-  //     const entries = await reader.getEntries();
-
-  //     console.log('Total entries : ' + entries.length)
-
-  //     for (const entry of entries) {
-  //       try {
-  //         this.filelist.push(entry.filename + "  " + entry.encrypted);
-  //         console.log(entry.filename + "  " + entry.encrypted);
+      if(password!==undefined && password!==null && password!==''){
+        reader = new zip.ZipReader(new zip.BlobReader(blob),{password});
+      }else{
+        reader = new zip.ZipReader(new zip.BlobReader(blob));
+      }
 
 
-  //         const x = await entry.getData(new zip.BlobWriter())
-  //         console.log(x);
+      const entries = await reader.getEntries();
 
-  //       } catch (error: any) {
-  //         if (error.message === zip.ERR_ENCRYPTED) {
-  //           console.log('Password Required')
-  //           return false;
-  //         } else if (error.message === zip.ERR_INVALID_PASSWORD) {
-  //           console.log('Invalid password');
-  //           return false;
-  //         } else {
-  //           throw error;
-  //         }
-  //       }
-  //     }
-  //     this.filelistToUI = this.filelist;
-  //     await reader.close();
-  //   } finally {
-  //     // await reader.close();
-  //   }
-  //   return true;
-  // };
+      console.log('Total entries : ' + entries.length)
+
+      for (const entry of entries) {
+        try {
+          this.filelist.push(entry.filename + "  " + entry.encrypted);
+          console.log(entry.filename + "  " + entry.encrypted);
+
+
+          const x = await entry.getData(new zip.BlobWriter())
+          console.log(x);
+
+        } catch (error: any) {
+          if (error.message === zip.ERR_ENCRYPTED) {
+            console.log('Password Required')
+            return false;
+          } else if (error.message === zip.ERR_INVALID_PASSWORD) {
+            console.log('Invalid password');
+            return false;
+          } else {
+            throw error;
+          }
+        }
+      }
+      this.filelistToUI = this.filelist;
+      await reader.close();
+    } finally {
+      // await reader.close();
+    }
+    return true;
+  };
 
 }
 
